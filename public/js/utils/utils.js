16aaa30070702c9e9cf9465b20b179c24d64ec13
arthur <arthureli81@gmail.com>
2025-08-31 17:29:37 -0600

    refactor: codigo de js archivo faults/faults.js refactorizado

--- public/js/utils/utils.js
+++ public/js/utils/utils.js
@@ -9,3 +9,28 @@ export function createURLWithParams(baseURL, filters) {
 
     return baseURL;
 }
+
+export function renderTableContent(objects, createRowFn) { // se encarga de crear un fragmento con todas las filas creadas por la funcion createRowFn
+    const fragment = document.createDocumentFragment();
+
+    objects.forEach(object => {
+        const row = createRowFn(object); // aqui se crean las filas
+
+        fragment.appendChild(row);
+    });
+
+    return fragment;
+}
+
+export async function getObjects(url, key) {
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        alert(data.error);
+        return false;
+    }
+
+    return data[key];
+}
